19149d7d4bb118a9d06c88020bc08cc3116eb030
Junsei <120575865+JuN-front@users.noreply.github.com>
2024-12-08 20:26:35 +0900

    Update SelectionSupportDataBase.java
    Deleted the added comments

--- src/main/java/ai/engineering/patternApplication/internal/entity/SelectionSupportDataBase.java
+++ src/main/java/ai/engineering/patternApplication/internal/entity/SelectionSupportDataBase.java
@@ -1,7 +1,6 @@
 package ai.engineering.patternApplication.internal.entity;
 
 import ai.engineering.patternApplication.internal.utility.*;
-/*This import cannot be used in astah-professional*/
 import com.change_vision.jude.api.gsn.editor.*;
 import com.change_vision.jude.api.inf.model.*;
 import com.change_vision.jude.api.inf.presentation.*;
@@ -17,8 +16,6 @@ public class SelectionSupportDataBase {
     public ArrayList<ArrayList<ArrayList<IElement>>> matchedAllIElements = new ArrayList<ArrayList<ArrayList<IElement>>>();
     public ArrayList<ArrayList<ArrayList<IElement>>> createdAllIElements = new ArrayList<ArrayList<ArrayList<IElement>>>();
 
-    /*Since Const, AstahAPIUtils, and AstahTransactionProcessing
-    are also dependent on system-safety in a different way, they must be fixed first.*/
     private Const constClass = new Const();
     private AstahAPIUtils astahAPIUtils = new AstahAPIUtils();
 
@@ -36,7 +33,6 @@ public class SelectionSupportDataBase {
 
     }
 
-    /*GsnModelEditor must be modified*/
     //要トランザクション
     public void UndoColorClearedPresentations(GsnModelEditor editor) throws Exception {
         //色を戻す
@@ -48,8 +44,6 @@ public class SelectionSupportDataBase {
             }
         }
 
-
-        /*Modify astahTransactionProcessing here*/
         //削除
         for(int i = 0; i < clearedPresentations.size(); i++) {
             astahTransactionProcessing.deleteElement(editor, clearedPresentations.get(i).getModel());
